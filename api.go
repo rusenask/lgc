6125bba8d53420cddbc653e3c731ff23066192d6
Karolis Rusenas <karolis.rusenas@opencredo.com>
2015-08-13 15:22:36 +0100

    basic implementation of stubbliest

---
+++ api.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+// getStubList calls to Stubo's REST API
+// /stubo/api/v2/scenarios/objects/{scenario_name}/stubs/detail
+func getStubList(scenario string) {
+	fmt.Println("got scenario to list stubs:", scenario)
+	resp, err := http.Get("http://localhost:8001/stubo/api/v2/scenarios/objects/" + scenario + "/stubs")
+	if err != nil {
+		fmt.Println("Got error: ", err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+	fmt.Printf("%s\n", string(body))
+
+}
