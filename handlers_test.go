96308bbe7eb065ad14a73a7033e7aceedd2f87c0
Karolis Rusenas <karolis.rusenas@opencredo.com>
2015-08-25 17:36:35 +0100

    test put stub handler with no scenario

--- handlers_test.go
+++ handlers_test.go
@@ -140,3 +140,25 @@ func TestPutStubsHandlerSuccess(t *testing.T) {
 
 	expect(t, respRec.Code, http.StatusOK)
 }
+
+func TestPutStubsHandlerNoScenario(t *testing.T) {
+	testData := `inserted`
+	server, c := testTools(201, testData)
+	m := setup(*c)
+
+	defer server.Close()
+
+	//Testing get scenario stubs
+	req, err := http.NewRequest("POST", "/stubo/api/put/stub?session=session",
+		strings.NewReader("anything here, proxy doesn't unmarshall it anyway"))
+	// no error is expected
+	expect(t, err, nil)
+
+	//The response recorder used to record HTTP responses
+	respRec := httptest.NewRecorder()
+
+	m.ServeHTTP(respRec, req)
+
+	expect(t, respRec.Code, http.StatusBadRequest)
+}
+
