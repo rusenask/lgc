2ff9cf90788a52a5015ca1ebde061eee93b78ea8
Karolis Rusenas <karolis.rusenas@opencredo.com>
2015-08-25 16:43:54 +0100

    test tools moved to more general file

--- api_test.go
+++ api_test.go
@@ -2,45 +2,9 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"net/http/httptest"
-	"net/url"
-	"reflect"
 	"strings"
 	"testing"
 )
-
-func expect(t *testing.T, a interface{}, b interface{}) {
-	if a != b {
-		t.Errorf("Expected %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
-	}
-}
-
-func refute(t *testing.T, a interface{}, b interface{}) {
-	if a == b {
-		t.Errorf("Did not expect %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
-	}
-}
-
-func testTools(code int, body string) (*httptest.Server, *Client) {
-
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(code)
-		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprintln(w, body)
-	}))
-
-	tr := &http.Transport{
-		Proxy: func(req *http.Request) (*url.URL, error) {
-			return url.Parse(server.URL)
-		},
-	}
-	httpClient := &http.Client{Transport: tr}
-
-	client := &Client{httpClient}
-	StuboURI = "http://localhost:3000"
-	return server, client
-}
 
 func TestGetScenarioStubs(t *testing.T) {
 	testData := `{"version":"1.2.3","data": [{"name": "scenario1"}]}`
