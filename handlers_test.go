f74ebf538636e9d22797bf5e608b7c4211ce9596
Karolis Rusenas <karolis.rusenas@opencredo.com>
2015-08-26 10:04:14 +0100

    begin session without mode

--- handlers_test.go
+++ handlers_test.go
@@ -337,3 +337,22 @@ func TestBeginSessionHandlerMissingScenario(t *testing.T) {
 	expect(t, respRec.Code, http.StatusBadRequest)
 }
 
+func TestBeginSessionHandlerMissingMode(t *testing.T) {
+	testData := `begin session`
+	server, c := testTools(200, testData)
+	m := setup(*c)
+
+	defer server.Close()
+
+	//Testing get all delay policies
+	req, err := http.NewRequest("GET", "/stubo/api/begin/session?session=session&scenario=sce1", nil)
+	// no error is expected
+	expect(t, err, nil)
+
+	//The response recorder used to record HTTP responses
+	respRec := httptest.NewRecorder()
+
+	m.ServeHTTP(respRec, req)
+
+	expect(t, respRec.Code, http.StatusBadRequest)
+}
